ee388d0ff726b215a7606e0fd171403b5919d8d4
George Akhmadeev <georgy.akhmadeev@yandex.ru>
2025-01-22 21:40:19 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,7 @@
+import telebot
+from tokens import token
+bot=telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.send_message(message.chat.id,"Привет ✌️ ")
+bot.infinity_polling()
